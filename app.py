a0c10fdd06b72b66a13007820ea01ea308f4f9ea
Henrik <henrik.aronsson.94@gmail.com>
2021-03-14 18:47:09 +0100

    Add user management

--- app.py
+++ app.py
@@ -1,42 +1,11 @@
-from flask import Flask, jsonify
 from flask_cors import CORS
 import connexion
 
-#app = Flask(__name__)
 app = connexion.App(__name__, specification_dir='./')
+
 app.add_api('swagger.yml')
 
 CORS(app.app)
-
-@app.route('/', methods=['GET'])
-def hello_world():
-    resp = {
-        "data": [
-            "hej",
-            "wops"
-        ]
-    }
-    return jsonify(resp)
-
-@app.route('/users/<id>')
-def load_user(id):
-    users = {
-        "1": {
-                "id": 1,
-                "name": "Henrik Aronsson",
-                "email": "henrik@mail.com"
-            },
-        "2": {
-                "id": 2,
-                "name": "Person Persson",
-                "email": "pers@mail.com"
-            }
-    }
-
-    if id in users:
-        return jsonify({ "data": users[id] })
-
-    return { "data": None }, 404, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- users.py
+++ users.py
@@ -20,3 +20,35 @@ def list_all():
         resp.append({ "id": USERS[userid]['id'], "name": USERS[userid]['name'] })
 
     return jsonify({ "data": resp })
+
+def get(id):
+    if id in USERS:
+        return jsonify({ "data": USERS[id] })
+
+    return { "data": None }, 404, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
+
+def update(id, user):
+    if id in USERS:
+        if "email" in user:
+            USERS[id]["email"] = user["email"]
+
+        if "name" in user:
+            USERS[id]["name"] = user["name"]
+
+        return jsonify(USERS[id])
+    
+    return { "data": None }, 404, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
+
+def add(user):
+    newId = int(max(USERS, key=int)) + 1
+
+    try:
+        USERS[str(newId)] = {
+            "id": newId,
+            "name": user["name"],
+            "email": user["email"]
+        }
+    except KeyError as keyErr:
+        return "Missing attribute " + str(keyErr), 400
+
+    return jsonify(USERS[str(newId)])
