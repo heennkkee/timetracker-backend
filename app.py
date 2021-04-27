b96ff299f83a31cf21f36f0699777e554eb1fc97
Henrik <henrik.aronsson.94@gmail.com>
2021-04-27 21:02:45 +0200

    some naive logging implemented

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = connexion.App(__name__, specification_dir='./')
 
 app.add_api('API-spec.yaml')
 
-CORS(app.app, origins=[ 'http://localhost:3000', 'http://localhost:5001' ], supports_credentials=True)
+CORS(app.app, origins=[ 'http://localhost:3000', 'http://localhost:5001', 'http://localhost:3001' ], supports_credentials=True)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
